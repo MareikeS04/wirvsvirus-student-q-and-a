f0ad23120b2bf7770c655a75172923cfe07db89f
Frederik Hummel <frederik.hummel@bumbledev.com>
2020-03-21 16:56:12 +0100

    #7 Add stylesheet

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { ClientContextProvider, createClient } from 'react-fetching-library';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import QuestionList from './pages/question-list';
+import './style.css';
 
 const App = () => (
   <ClientContextProvider client={createClient({})}>
     <BrowserRouter>
-    <Switch>
-      <Route exact path="/questions" component={QuestionList} />
-    </Switch>
+      <Switch>
+        <Route exact path="/questions" component={QuestionList} />
+      </Switch>
     </BrowserRouter>
   </ClientContextProvider>
 );
